b1ef00ad146eca68884134e8b04b53bfda9ef94f
luo <479933015@qq.com>
2022-12-17 23:23:27 +0800

    更新插件支持

--- src/extension.ts
+++ src/extension.ts
@@ -6,17 +6,6 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ling.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ling!');
-	});
-
-	context.subscriptions.push(disposable);
-
 	/** 按住ctrl 识别php中的string类的字符串支持跳转 */
 	let stringClass = vscode.languages.registerDefinitionProvider(['php'], {provideDefinition})
 	context.subscriptions.push(stringClass)
@@ -84,9 +73,20 @@ export function activate(context: vscode.ExtensionContext) {
 // This method is called when your extension is deactivated
 export function deactivate() {}
 
-
+const symbol_map:any = {
+	'->': {
+		php: '->',
+		typescript:  '.',
+		javascript:  '.',
+		vue: '.',
+		python: '.'
+	}
+}
 function shortcutRule(symbol: string, ctrl = false){
 	return (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
+		if(symbol in symbol_map && textEditor.document.languageId in symbol_map[symbol]){
+			symbol = symbol_map[symbol][textEditor.document.languageId]
+		}
 		let line = textEditor.document.lineAt(textEditor.selection.end.line)
 		vscode.commands.executeCommand('cursorLineEnd')
 		let text = line.text
